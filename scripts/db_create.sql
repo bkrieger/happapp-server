14bfe1c6e5b0f76bae812fcb06bbd3ad15fcd8d7
Alex Yau <ayau@sas.upenn.edu>
2013-11-14 23:59:31 -0500

    updated query

--- scripts/db_create.sql
+++ scripts/db_create.sql
@@ -1,6 +1,11 @@
 DROP TABLE users;
 CREATE TABLE users (
 id int NOT NULL AUTO_INCREMENT,
-phone_number varchar(50) NOT NULL,
+phone_number CHARACTER(10) NOT NULL,
+os BINARY,
+pn_enabled BOOLEAN DEFAULT FALSE, 
 PRIMARY KEY (id)
 );
+
+-- TEST
+-- INSERT INTO users (phone_number, os) VALUES ("6969696969", 1);
