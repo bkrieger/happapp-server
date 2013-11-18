1677119656ff6f143eaad2bc57d28a971ed04ddd
Brandon Krieger <brandontkrieger@gmail.com>
2013-11-17 22:36:54 -0500

    sql comment

--- scripts/db_create.sql
+++ scripts/db_create.sql
@@ -7,7 +7,7 @@ pn_token VARCHAR(255),
 PRIMARY KEY (id)
 );
 
-
+-- All times are in UTC
 DROP TABLE happs_by_day;
 CREATE TABLE happs_by_day (
 id int NOT NULL AUTO_INCREMENT,
@@ -16,6 +16,9 @@ counter int DEFAULT 0,
 PRIMARY KEY (id)
 );
 
+-- All times are in UTC
+-- To convert from period to time, do: period*.25. This gives you the number of hours after midnight.
+-- For example, period 10 -> 10*.25 = 2.5 -> 2:30am - 2:45am. period 80 -> 80*.25 = 20 -> 8pm - 8:15pm 
 DROP TABLE happs_within_day;
 CREATE TABLE happs_within_day (
 period int NOT NULL AUTO_INCREMENT,
